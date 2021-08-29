805c1de163651bdf0196d77d53a63d04747530a6
4molybdenum2 <tathagatapaul7@gmail.com>
2021-08-29 23:49:59 +0530

    changes to assignments page

--- frontend/src/components/UserCore/Assignment.js
+++ frontend/src/components/UserCore/Assignment.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
+import Container from "@material-ui/core/Container";
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
@@ -53,34 +54,36 @@ export default function Assignment() {
   }, [assignments, id]);
 
   return (
-    <Grid item xs={12} md={8}>
-      {assignments.map((assignment) => (
-          <Card className={classes.root}>
-          <CardActionArea>
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                {assignment.title}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                {assignment.description}
-              </Typography>
-              <Typography variant="body3" color="textSecondary" component="p">
-                {assignment.dueDate}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Submit
-            </Button>
-            <Button size="small" color="primary">
-              Info
-            </Button>
-          </CardActions>
-        </Card>
-      ))}
-    </Grid>
-
+    <Container>
+      <h1>Assignments: </h1>
+      <Grid item xs={12} md={8}>
+        {assignments.map((assignment) => (
+            <Card className={classes.root}>
+            <CardActionArea>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {assignment.title}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  {assignment.description}
+                </Typography>
+                <Typography variant="body3" color="textSecondary" component="p">
+                  {assignment.dueDate}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Submit
+              </Button>
+              <Button size="small" color="primary">
+                Info
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+      </Grid>
+    </Container>
 
   );
 }
